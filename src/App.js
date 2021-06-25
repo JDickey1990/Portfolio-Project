29f446d3a2e38f859edc389cbf595fd3f66e8d6e
jmd <jordan.michael.dickey@gmail.com>
2021-06-25 14:00:38 -0600

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+// import Particles from "react-particles-js"
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <>
+        <Navbar/>
+        <Header/>
+ 
+      </>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Typed from 'react-typed';
+import Particles from "react-particles-js";
+
+const Header = () => {
+    return (
+      <div className="header-wrapper">
+            <Particles 
+              className="particles-canvas"
+              params={{
+                particles: {
+                  number: {
+                    value: 50,
+                  },
+                  color: {
+                    value: ['#141111']
+                  },
+                  line_linked: {
+                    color: '#fcf7f7',
+                    opacity: 1
+                  }
+                }
+              }}
+            />
+          <div className="main-info">
+            {/* <h1></h1> */}
+            <Typed
+                className="typed-text"
+                strings={["Full Stack Software Engineering", "JavaScript", "React", "Redux", "Ruby", "Ruby on Rails", "HTML", "CSS", "Bootstrap"]}
+                typeSpeed={30}
+                backSpeed={60}
+                loop
+            />
+            <a href="#" className="btn-main-heading">Contact Me </a>
+          </div>
+      </div> 
+    )
+}
+
+export default Header
