ab06c35d1183eb0518817525aee2e3822f907d91
jmd <jordan.michael.dickey@gmail.com>
2021-07-03 19:29:34 -0600

    Creating the Testimonials component and installing the react carousels library

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="testimonials">
+      <h1>Kind Words</h1>
+      <div className="container">
+        <div className="testimonials-content"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
---
+++ src/Components/TestimonialsCarousel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestimonialsCarousel = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TestimonialsCarousel
